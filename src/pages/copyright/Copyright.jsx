5d518e7d5e56c2563d4e840615e185220ac65c2b
enztexier <enzotexier.studio@gmail.com>
2023-04-27 11:45:34 +0200

    routing & pages

---
+++ src/pages/copyright/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Copyright = () => {
+    return (
+        <div>
+            <h2>copyright</h2>
+            
+        </div>
+    );
+};
+
+export default Copyright;
